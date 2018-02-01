f724920dcbf8b578570dce08364121aba56992e0
carlostrapet <trapet_llamas@hotmail.com>
2018-02-01 15:26:27 +0000

    added station attributes. all tests passing

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,11 +2,16 @@ require 'station'
 
 describe Station do
 
-  subject(:station) { described_class.new(name) }
+  subject(:station) { described_class.new(:aldgate, 2) }
 
   describe "#initialize" do
+
     it "store a name" do
-    expect(station.name).to eq name
+    expect(station.name).to eq :aldgate
+    end
+
+    it "stores a zone" do 
+      expect(station.zone).to eq 2
+    end
   end
-end
 end
